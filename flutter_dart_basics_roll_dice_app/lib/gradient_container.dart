98474c5ac025922f655bb367a9ca72b3fff39733
anowakowski <nowakowski.tools@gmail.com>
2024-03-07 12:19:05 +0100

    add more logic to widgets

--- flutter_dart_basics_roll_dice_app/lib/gradient_container.dart
+++ flutter_dart_basics_roll_dice_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dart_basics1/styled_text.dart';
 
+const startAlingment = Alignment.topLeft;
+const endAlingment = Alignment.bottomCenter;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   
@@ -10,12 +13,12 @@ class GradientContainer extends StatelessWidget {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
             colors: [Color.fromARGB(255, 26, 2, 80), Color.fromARGB(255, 45, 7, 98)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomCenter
+            begin: startAlingment,
+            end: endAlingment
         )
       ),
       child: const Center(
-        child: StyledText()
+        child: StyledText('test text')
       ),
     );
   }
