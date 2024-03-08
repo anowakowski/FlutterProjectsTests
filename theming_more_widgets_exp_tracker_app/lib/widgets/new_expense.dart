7902a2335a48b4ae5e4333aa378f5d222d90e5c6
anowakowski <nowakowski.tools@gmail.com>
2024-03-08 17:48:19 +0100

    added more for new expense widget

--- theming_more_widgets_exp_tracker_app/lib/widgets/new_expense.dart
+++ theming_more_widgets_exp_tracker_app/lib/widgets/new_expense.dart
@@ -10,19 +10,78 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: now, initialDate: now);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            //keyboardType: TextInputType.name,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title')
             ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount')
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16,),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Selected date'),
+                    IconButton(
+                      onPressed: _presentDatePicker, 
+                      icon: const Icon(Icons.calendar_month)
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                }, 
+                child: const Text('Cancel')
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                }, 
+                child: const Text('Save Expense')
+              )
+            ],
           )
         ],
       ),
