14345bcfa7f123a906765f88134090e21697f4fe
anowakowski <nowakowski.tools@gmail.com>
2024-03-08 13:43:22 +0100

    added first part of expensess

---
+++ theming_more_widgets_exp_tracker_app/lib/expensess.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Expensess extends StatefulWidget {
+  const Expensess({super.key});
+
+  @override
+  State<Expensess> createState() {
+    return _ExpensessState();
+  }
+}
+
+class _ExpensessState extends State<Expensess> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
