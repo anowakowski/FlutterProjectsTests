52a5a18705d6cd35896dbb5f603b12ca753e2257
anowakowski <nowakowski.tools@gmail.com>
2024-03-08 14:26:05 +0100

    added expenses list

--- theming_more_widgets_exp_tracker_app/lib/expensess.dart
+++ theming_more_widgets_exp_tracker_app/lib/expensess.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:theming_more_widgets_exp_tracker_app/models/expense.dart';
 
 class Expensess extends StatefulWidget {
   const Expensess({super.key});
@@ -10,6 +11,12 @@ class Expensess extends StatefulWidget {
 }
 
 class _ExpensessState extends State<Expensess> {
+  final List<Expense> _registeredExpensess = [
+    Expense(title: 'Flutter Course', amanount: 19.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amanount: 15.69, date: DateTime.now(), category: Category.leisuer)
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- theming_more_widgets_exp_tracker_app/lib/models/expense.dart
+++ theming_more_widgets_exp_tracker_app/lib/models/expense.dart
@@ -3,10 +3,18 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class Expense {
-  Expense({required this.title, required this.amanount, required this.date}): id = uuid.v4();
+  Expense({required this.title, required this.amanount, required this.date, required this.category}): id = uuid.v4();
 
   final String id;
   final String title;
   final double amanount;
   final DateTime date;
+  final Category category;
+}
+
+enum Category {
+  food,
+  traver,
+  leisuer,
+  work
 }
