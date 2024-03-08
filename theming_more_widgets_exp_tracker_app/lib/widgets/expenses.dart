24f80d5caef13f959f3cd1abec0575b21c0a61c6
anowakowski <nowakowski.tools@gmail.com>
2024-03-08 15:52:28 +0100

    added new expense widget

--- theming_more_widgets_exp_tracker_app/lib/widgets/expenses.dart
+++ theming_more_widgets_exp_tracker_app/lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:theming_more_widgets_exp_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:theming_more_widgets_exp_tracker_app/models/expense.dart';
+import 'package:theming_more_widgets_exp_tracker_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -17,13 +18,17 @@ class _ExpensesState extends State<Expenses> {
     Expense(title: 'Cinema', amanount: 15.69, date: DateTime.now(), category: Category.leisuer)
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
