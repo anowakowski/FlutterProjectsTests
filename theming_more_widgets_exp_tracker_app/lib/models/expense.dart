91f077d51c2684111a50f78fac6420bbf1ebe433
anowakowski <nowakowski.tools@gmail.com>
2024-03-08 13:58:44 +0100

    added uuid and using in expense

--- theming_more_widgets_exp_tracker_app/lib/models/expense.dart
+++ theming_more_widgets_exp_tracker_app/lib/models/expense.dart
@@ -1,5 +1,9 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 class Expense {
-  Expense({required this.id, required this.title, required this.amanount, required this.date});
+  Expense({required this.title, required this.amanount, required this.date}): id = uuid.v4();
 
   final String id;
   final String title;
