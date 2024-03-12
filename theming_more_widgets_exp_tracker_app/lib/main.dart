bcfa0d78589ef649bee7d6418afec74d7911930e
anowakowski <nowakowski.tools@gmail.com>
2024-03-12 10:41:09 +0100

    first set theme for app

--- theming_more_widgets_exp_tracker_app/lib/main.dart
+++ theming_more_widgets_exp_tracker_app/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:theming_more_widgets_exp_tracker_app/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
+
 void main() {
   runApp (
-    const MaterialApp (
+     MaterialApp (
       //theme: ThemeData(useMaterial3: true),
-      home: Expenses(),
+      theme: ThemeData().copyWith(
+          //scaffoldBackgroundColor: const Color.fromARGB(255, 245, 165, 159),
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer
+          )
+      ),
+      home: const Expenses(),
     ),
   );
 }
