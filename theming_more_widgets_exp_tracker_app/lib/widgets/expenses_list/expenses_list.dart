c1655fe94dc830afbf77f6d7854356f000caa5a9
anowakowski <nowakowski.tools@gmail.com>
2024-03-12 11:06:10 +0100

    next parts of theme

--- theming_more_widgets_exp_tracker_app/lib/widgets/expenses_list/expenses_list.dart
+++ theming_more_widgets_exp_tracker_app/lib/widgets/expenses_list/expenses_list.dart
@@ -16,6 +16,10 @@ class ExpensesList extends StatelessWidget {
       itemBuilder: (ctx, index) {
         return Dismissible(
           key: ValueKey(expenses[index]),
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.75), 
+            margin: Theme.of(context).cardTheme.margin,
+          ),
           onDismissed: (direction) {
             onRemoveExpense(expenses[index]);
           },
