a2fec8e7e3a7965ceb164e02914044765e0e379f
anowakowski <nowakowski.tools@gmail.com>
2024-03-13 15:03:24 +0100

    more meal details

--- multi_screen_navigation_meals_app/lib/screens/meal_details.dart
+++ multi_screen_navigation_meals_app/lib/screens/meal_details.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 import 'package:multi_screen_navigation_meals_app/models/meal.dart';
@@ -13,7 +15,52 @@ class MealDetails extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl, width: double.infinity, height: 300, fit: BoxFit.cover),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl, 
+              width: double.infinity, 
+              height: 300, 
+              fit: BoxFit.cover
+            ),
+            const SizedBox(height: 14),
+            Text(
+              'Ingridients', 
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            const SizedBox(height: 14),
+            for (final ingrindient in meal.ingredients)
+              Text(
+                ingrindient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground)
+              ),
+            const SizedBox(height: 14),
+            Text(
+              'Steps', 
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            const SizedBox(height: 14),
+            for (final step in meal.steps)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground)
+                ),
+              ),                      
+          ],
+        ),
+      ),
     );
   }
 
